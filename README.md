e6cf13d532929b2319857e20787093180a9c7ea1
yoav mashraki <yoavmashraki@walla.com>
2023-05-12 10:39:49 +0300

    Init Project

---
+++ README.md
@@ -0,0 +1,1 @@
+Poker Project in UE5.2 for Oculus Quest 2 in Multiplayer
---
+++ YMPoker/Source/YMPoker/YMPokerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YMPokerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YMPOKER_API AYMPokerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
